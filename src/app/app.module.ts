b7a87b94960d5a7314e37354ca35b2e7a4923ae9
Emerald Elnas <emerald@hyperstacksinc.com>
2021-04-06 17:36:54 +0800

    add main screen ui only

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,22 @@ import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { MainComponent } from './pages/main/main.component';
+import { TodaySalesComponent } from './pages/main/today-sales/today-sales.component';
+import { TransactionsComponent } from './pages/main/transactions/transactions.component';
+import { TransactionComponent } from './pages/main/transactions/transaction/transaction.component';
+import { ProductsComponent } from './pages/main/products/products.component';
+import { ProductComponent } from './pages/main/products/product/product.component';
 
 @NgModule({
-  declarations: [AppComponent, MainComponent],
+  declarations: [
+    AppComponent,
+    MainComponent,
+    TodaySalesComponent,
+    TransactionsComponent,
+    TransactionComponent,
+    ProductComponent,
+    ProductsComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/pages/main/today-sales/today-sales.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-today-sales',
+  templateUrl: './today-sales.component.html',
+  styleUrls: ['./today-sales.component.scss']
+})
+export class TodaySalesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
